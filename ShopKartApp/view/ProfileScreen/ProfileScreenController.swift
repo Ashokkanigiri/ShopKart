968c3bed283691d40557e0476c0a6a0a02df4e98
Ashokkanigiri <ashkanigiri@gmail.com>
2022-11-22 14:56:02 +0530

    Added profile screen

---
+++ ShopKartApp/view/ProfileScreen/ProfileScreenController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileScreenController.swift
+//  ShopKartApp
+//
+//  Created by Ashok Kanigiri on 22/11/22.
+//
+
+import Foundation
+import UIKit
+
+class ProfileScreenController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
